96bb02e13e7114bd1d4c7a3da1bb479757a10297
jford49 <jford49@users.noreply.github.com>
2022-11-20 10:11:06 -0500

    Update stable_diffusion.py

--- stable_diffusion_tf/stable_diffusion.py
+++ stable_diffusion_tf/stable_diffusion.py
@@ -162,14 +162,19 @@ class StableDiffusion:
             if singles:
                 decoded = self.decode_latent(latent)#, input_image_array, input_mask_array)
                 out_list.append((decoded, "latent"))
-                if latent_orgin is not None:
+                
+                if input_mask is not None and input_image is not None:
+                    decoded = self.decode_latent(latent, input_image_array, input_mask_array)
+                    out_list.append((decoded, "latent masked"))
+                
+                s = '''if latent_orgin is not None:
                     decoded = self.decode_latent(latent_orgin)#, input_image_array, input_mask_array)
                     out_list.append((decoded, "latent_orgin"))
                     
                 if mix is not None:
                     mix = ((mix + 1) / 2) * 255            
                     mix = np.clip(mix, 0, 255)[0,:,:,:].astype("uint8")
-                    out_list.append((mix, "mix"))
+                    out_list.append((mix, "mix"))#'''
                 
         if not singles:
             decoded = self.decode_latent(latent, input_image_array, input_mask_array)
